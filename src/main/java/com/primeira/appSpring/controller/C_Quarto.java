67452bcf094fe0f1de1bd0f0eb57b5b18c1ed014
Lucas Fernandes Braz <lucas.braz@sistemafiep.org.br>
2025-01-29 21:36:53 -0300

    Removed unecessary imports

--- src/main/java/com/primeira/appSpring/controller/C_Quarto.java
+++ src/main/java/com/primeira/appSpring/controller/C_Quarto.java
@@ -2,7 +2,6 @@ package com.primeira.appSpring.controller;
 
 import com.primeira.appSpring.service.S_Quarto;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
--- src/main/java/com/primeira/appSpring/controller/C_Reserva.java
+++ src/main/java/com/primeira/appSpring/controller/C_Reserva.java
@@ -4,7 +4,6 @@ import com.primeira.appSpring.model.M_Resposta;
 import com.primeira.appSpring.model.M_Usuario;
 import com.primeira.appSpring.service.S_Reserva;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
