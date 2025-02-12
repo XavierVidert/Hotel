f223b00a9956d3506ad41809508a7a53878d11ea
Aluno <xaviervidrt@gmail.com>
2025-02-12 16:31:25 -0300

    salvo13

--- src/main/java/com/primeira/appSpring/repository/R_Locacao.java
+++ src/main/java/com/primeira/appSpring/repository/R_Locacao.java
@@ -40,6 +40,7 @@ public interface R_Locacao extends JpaRepository<M_Locacao, Long> {
             "on locacao.id_quarto = quarto.id " +
             "LEFT JOIN consumo " +
             "on locacao.id = consumo.id_locacao " +
+            "and consumo.id_locacao !=2 "+
             "where locacao.id_usuario = :id_usuario " +
             "and now() between locacao.check_in and locacao.check_out " +
             "GROUP BY 1,2,3,4,5,6,7 " +
@@ -63,6 +64,7 @@ public interface R_Locacao extends JpaRepository<M_Locacao, Long> {
             "on locacao.id_quarto = quarto.id " +
             "LEFT JOIN consumo " +
             "on locacao.id = consumo.id_locacao " +
+            "and consumo.id_produto != 2 "+
             "where locacao.id_usuario = :id_usuario " +
             "and now() < locacao.check_in " +
             "GROUP BY 1,2,3,4,5,6,7 " +
@@ -86,6 +88,7 @@ public interface R_Locacao extends JpaRepository<M_Locacao, Long> {
             "on locacao.id_quarto = quarto.id " +
             "LEFT JOIN consumo " +
             "on locacao.id = consumo.id_locacao " +
+            "and consumo.id_produto != 2 " +
             "where locacao.id_usuario = :id_usuario " +
             "and now() > locacao.check_out " +
             "GROUP BY 1,2,3,4,5,6,7 " +
@@ -95,6 +98,6 @@ public interface R_Locacao extends JpaRepository<M_Locacao, Long> {
     @Query(value="select * " +
             "from locacao " +
             "where (cast(check_out as date) > now() or cast(check_out as date) = cast(check_in as date)) " +
-            "and now() between check_in and check_out",nativeQuery = true)
+            "and cast(now() as date) between cast(check_in as date) and cast(check_out as date)",nativeQuery = true)
     List<M_Locacao> getLocacoesGerarDiarias();
 }
